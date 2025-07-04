c1a90fad8f6ec8400c7bdc67c13f2e8db29fe44c
christiannataren <cnatarnlaguna@byupathway.edu>
2025-07-04 18:04:59 -0400

    assigments finalized

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        String name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static String PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        String name = Console.ReadLine();
+        return name;
+
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(String name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
