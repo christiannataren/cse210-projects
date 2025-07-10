a00ef9dd112d7bde5e2816a6d55652fadaa53e17
christiannataren <cnatarnlaguna@byupathway.edu>
2025-07-10 13:38:09 -0400

    resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job = new Job();
+        job._company = "Microsoft";
+        job._jobTitle = "Software Engineer";
+        job._startYear = 2020;
+        job._endYear = 2024;
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Web Designer";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+        Resume resume = new Resume();
+        resume._name = "Christian Nataren Laguna";
+        resume._jobs.Add(job2);
+        resume._jobs.Add(job);
+
+        resume.Display();
     }
 }
