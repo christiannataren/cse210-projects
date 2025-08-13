d6570864a1010159dcff8ae0dca50b9a4825dbe9
christiannataren <cnatarnlaguna@byupathway.edu>
2025-08-12 20:43:37 -0400

    exceed

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,4 @@
+////////////To exceed requirements, I've added a system to load goals from a list of files on a menu, as well as the option to delete any required file from the menu options.
 using System;
 
 class Program
